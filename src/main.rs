b8fbeef845fefa6296223065adf4d73a7da58fc0
raimon <raimon49@hotmail.com>
2022-02-11 15:23:15 +0900

    Add a code about implement self defined assertion macro

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 macro_rules! my_assert_eq {
     ($left:expr , $right:expr) => ({
-
+        match (&$left, &$right) {
+            (left_val, right_val) => {
+                if !(*left_val == *right_val) {
+                    panic!("assertion failed: `(left == right)` (left: `{:?}`, right: `{:?}`)", left_val, right_val);
+                }
+            }
+        }
     });
 }
 fn main() {
