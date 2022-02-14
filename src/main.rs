be550bd1eedc031e3e4d2f61758a1f51733e6023
raimon <raimon49@hotmail.com>
2022-02-14 20:05:24 +0900

    Add an annotate comment about pattern and template of macro

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 macro_rules! my_assert_eq {
+    // パターンを定義し、パターンマッチした対象に適用するテンプレートを書く
     ($left:expr , $right:expr) => ({
+        // {} の中身がテンプレート
         match (&$left, &$right) {
             (left_val, right_val) => {
                 if !(*left_val == *right_val) {
@@ -10,5 +12,5 @@ macro_rules! my_assert_eq {
     });
 }
 fn main() {
-    my_assert_eq!(1, 2);
+    my_assert_eq!(1, 1);
 }
