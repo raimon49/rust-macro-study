285864a84f88b1985cdf8c20a291d28dd53c35de
raimon <raimon49@hotmail.com>
2022-02-17 19:26:12 +0900

    Add a code about call self defined macro with function

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,21 @@ macro_rules! my_assert_eq {
         }
     });
 }
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
 fn main() {
     my_assert_eq!(1, 1);
+    my_assert_eq!(gcd(6, 10), 2);
 }
