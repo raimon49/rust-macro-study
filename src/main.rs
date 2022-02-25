c936da1e2de3e7bde0a8bd97fc714f4297a9720c
raimon <raimon49@hotmail.com>
2022-02-25 21:53:26 +0900

    Add an annotate comment about call macro

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ fn main() {
     let vec_1 = vec![1, 2, 3];
     let vec_2 = vec!(1, 2, 3);
     let vec_3 = vec!{1, 2, 3};
-    assert_eq!(vec_1, vec_2);
+    assert_eq!(vec_1, vec_2); // assert_eqマクロを()で呼ぶのも慣例で、実際は[]でも{}でも呼び出せる
     assert_eq![vec_1, vec_3];
     assert_eq!{vec_2, vec_3}
 }
